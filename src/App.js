df1000336637b4b64a921b7d30f59bfbf6515623
sam-schmitt <samuelgschmitt@gmail.com>
2022-01-18 00:02:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Audio from "./assets/audio/START SCREEN.mp3";
+import { useState } from "react";
+import {
+	Card,
+	Container,
+	FormControl,
+	makeStyles,
+	TextField,
+	Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+	container: {
+		display: "flex",
+		flexDirection: "column",
+		alightItems: "center",
+		justifyContent: "center",
+		padding: 5,
+	},
+	card: {
+		display: "flex",
+		flexDirection: "column",
+		alightItems: "center",
+		justifyContent: "center",
+		margin: "20px auto",
+		padding: 5,
+		backgroundColor: "#F2F2F2",
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const styles = useStyles();
+	async function fileToDataURL(file) {
+		var reader = new FileReader();
+		return {
+			file: await new Promise(function (resolve, reject) {
+				reader.onload = function (event) {
+					resolve(event.target.result);
+				};
+				reader.readAsDataURL(file);
+			}),
+			name: file.name,
+		};
+	}
+
+	function getDataURLs(target) {
+		return Promise.all([...target.files].map(fileToDataURL));
+	}
+	const upload = async (e) => {
+		let newFiles = await getDataURLs(e.target);
+		setFilteredFiles(newFiles);
+		setFiles(newFiles);
+	};
+	const [files, setFiles] = useState([]);
+	const [filteredFiles, setFilteredFiles] = useState([]);
+	const [fileName, setFileName] = useState([]);
+
+	const searchFilterFunction = async (text) => {
+		if (text) {
+			const newData = files.filter(function (item) {
+				const itemData = item.name ? item.name.toUpperCase() : "".toUpperCase();
+				const textData = text.toUpperCase();
+				return itemData.indexOf(textData) > -1;
+			});
+			setFilteredFiles(newData);
+			setFileName(text);
+		} else if (text == "") {
+			setFileName("");
+			setFilteredFiles(files);
+		}
+	};
+	return (
+		<Container maxWidth='md' className={styles.container}>
+			<Typography variant='h1'>SoundBites</Typography>
+			<FormControl>
+				<input
+					type='file'
+					multiple
+					onChange={(e) => {
+						upload(e);
+					}}
+				/>
+				{files.length > 1 && (
+					<TextField
+						label={"Day title..."}
+						color={"primary"}
+						variant={"filled"}
+						onChange={(e) => {
+							searchFilterFunction(e.target.value);
+						}}
+						value={fileName}
+					/>
+				)}
+			</FormControl>
+
+			{filteredFiles.map(function (item, index) {
+				return (
+					<Card className={styles.card}>
+						<Typography>{item.name}</Typography>
+						<audio controls>
+							<source src={item.file} type='audio/mpeg' />
+							Your browser does not support the audio tag.
+						</audio>
+					</Card>
+				);
+			})}
+		</Container>
+	);
 }
 
 export default App;
